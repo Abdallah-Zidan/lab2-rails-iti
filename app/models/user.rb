9798a1fb1d6953781205181e543b51b54bee3707
Abd Allah Zidan <eng.abdallahzidan@gmail.com>
2020-04-26 16:14:38 +0200

    adding lab1 to this repo

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :articles
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
